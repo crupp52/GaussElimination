c3bf5f8a0f568367171d7b0aa59afe8bc6ee42fa
zucklevente <zuck.levente@protonmail.com>
2019-04-06 14:56:15 +0200

    Add: ChangeRows method

---
+++ GaussElimination/GaussElimination/BL/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussElimination
+{
+    public class Logic
+    {
+        void ChangeRows(ref double[,] matrix, int firstRowID, int secondRowID)
+        {
+            double[] temp = new double[matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                temp[i] = matrix[firstRowID, i];
+                matrix[firstRowID, i] = matrix[secondRowID, i];
+                matrix[secondRowID, i] = temp[i];
+            }
+        }
+    }
+}
---
+++ GaussElimination/GaussElimination/Display/DisplayTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussElimination
+{
+    public class DisplayTools
+    {
+        public void ToConsole(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                }
+            }
+        }
+    }
+}
